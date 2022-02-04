70fe82f072bb0c6842dc0f4e3b0d6b65f06132c4
Rohanc28 <81807980+Rohanc28@users.noreply.github.com>
2022-02-04 23:12:59 +0530

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,10 +1,13 @@
-#import db
+# import db
 import cv2
 import PIL
+import time
+import multiprocessing
+from matplotlib.pyplot import bar
 from pyzbar import pyzbar
 
 
-def read_barcodes(frame):
+def read_barcodes(frame, plate):
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         x, y, w, h = barcode.rect
@@ -17,19 +20,32 @@ def read_barcodes(frame):
         cv2.putText(frame, barcode_info, (x + 3, y - 6),
                     font, 1.5, (255, 255, 255), 1)
         # 3
-        with open("barcode_result.txt", mode='w') as file:
-            file.write("ID:" + barcode_info)
+        with open("barcode_result.txt", mode='w+') as file:
+            f = str(file.readlines())
+            #print("f "+str(f[3:]))
+            #print("plate "+str(plate))
+            #print("barcode_info "+str(barcode_info)[:-10])
+            if str(barcode_info)[:-10] == str(plate):
+                print("MATCH!\n"+barcode_info)
+                file.write("ID:" + str(barcode_info))
+                file.close()
+                exit()
+            file.close()
+            # cv2.VideoCapture(0).release()
+            # cv2.destroyAllWindows
+            # quit()
+            # return
     return frame
 
 
-def main():
+def camera(plate):
     # 1
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
     # 2
     while ret:
         ret, frame = camera.read()
-        frame = read_barcodes(frame)
+        frame = read_barcodes(frame, str(plate))
         cv2.imshow('Ticket Scanner', frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
@@ -38,6 +54,13 @@ def main():
     cv2.destroyAllWindows()
 
 
+def capture(plate):
+    p = multiprocessing.Process(target=camera, name="cam", args=(plate,))
+    p.start()
+    time.sleep(8)
+    p.terminate()
+    p.join()
+
 # 4
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#    capture("RJ01AB1234")
