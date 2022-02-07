b282361fccdecbe5ea5676bdf6ebff14544e0a73
Rohanc28 <81807980+Rohanc28@users.noreply.github.com>
2022-02-07 19:27:34 +0530

    Update exit_check.py

--- exit_check.py
+++ exit_check.py
@@ -15,29 +15,16 @@ def chexit(id, carp):
     #
     plate = hash[:10]
     curr_time = ticket_gen.dater()[-4:]
-    print()
-    print("Matching... \nCar number plate: "+plate+"\nDate: " +
+    #print()
+    #print("Matching... \nCar number plate: "+plate+"\nDate: " +
           hash[-10:-8]+"-"+hash[-8:-6]+"-20"+hash[-6:-4])
     print()
     #print(str(carp).upper() + " vs " + plate)
     if str(carp).upper() == plate:
-        print("Match confirmed")
+        #print("Match confirmed")
         fee = (int(curr_time)-int(hash[-4:]))
         fee /= 4
         return (fee)
     else:
         print("Invalid")
         return("Invalid")
-
-    """
-    key = input("Press 'Y' to Create Fee\nPress 'N' to Cancel\n\n:")
-    if key == 'n' or key == 'N':
-        print("Incorrect Match")
-        #print('date: '+hash[-10:-8]+"-"+hash[-8:-6]+"-20"+hash[-6:-4])
-        #print('time: '+hash[-4:-2]+":"+hash[-2:])
-    elif key == 'y' or key == 'Y':
-        print("Match confirmed by user\nGenerating Parking fee")
-        #print("INR: "+str(int(curr_time)-int(hash[-4:]))+"/-")
-    """
-
-    #print('plate: '+hash[:10])
